dce4a24058a648c8ccbc463bdc87482544007103
peshopbs2 <peshopbs2@gmail.com>
2024-02-12 09:41:46 +0200

    Create eratosten.c

---
+++ eratosten.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+    int x;
+    scanf("%d", &x);
+    
+    bool* arr = (bool*) malloc( (x+1) * sizeof(bool));
+    
+    memset(arr, false, (x+1) * sizeof(bool));
+    
+    for(int i = 2; i <= x; i++)
+    {
+        for(int j = i * 2; j <= x; j += i)
+        {
+            arr[j] = true;
+        }
+    }
+    
+    for(int i = 2; i <= x; i++)
+    {
+        if(!arr[i]) // arr[i] == false
+        {
+            printf("%d ", i);
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
